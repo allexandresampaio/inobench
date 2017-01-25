9b004f6227f1ba1c1ecb680f38a3997b1d67bb4b
allexandresampaio <allexandresss@gmail.com>
2017-01-25 15:13:15 -0300

    refatoração em novo projeto java

---
+++ NosqlBench/src/leituraCSV/LeituraCsv.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leituraCSV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import org.bson.Document;
+
+/**
+ *
+ * @author Allexandre
+ */
+public class LeituraCsv {
+
+    ArrayList<Document> documentos = new ArrayList<>();
+    
+    private void criaDocumentos() throws IOException {
+        //String path = new File("src/arquivos/AirQualityUCI.csv").getCanonicalPath();
+        //BufferedReader br = new BufferedReader(new FileReader(path));
+        InputStream is = LeituraCsv.class.getResourceAsStream("/arquivos/AirQualityUCI.csv");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        
+        String line = "";
+
+        while ((line = br.readLine()) != null) {
+            String[] linha = line.split(";");
+            this.insereDocumento(linha);
+        }
+    }
+
+    private void insereDocumento(String[] linha) {
+        Document documento = new Document();
+        documento.append("date", linha[0]).
+                append("time", linha[1]).
+                append("s1", linha[2]).
+                append("s2", linha[3]).
+                append("s3", linha[4]).
+                append("s4", linha[5]).
+                append("s5", linha[6]).
+                append("s6", linha[7]).
+                append("s7", linha[8]).
+                append("s8", linha[9]).
+                append("s9", linha[10]).
+                append("s10", linha[11]).
+                append("s11", linha[12]).
+                append("s12", linha[13]).
+                append("s13", linha[14]);
+
+        documentos.add(documento);
+    }
+
+    public ArrayList<Document> getDocumentos() throws IOException {
+        this.criaDocumentos();
+        return documentos;
+    }
+}
---
+++ NosqlBench/src/mongodb/FachadaMongo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mongodb;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ *
+ * @author Allexandre
+ */
+public class FachadaMongo {
+
+    private static FachadaMongo instancia = null;
+
+//    private static final String HOST = "localhost";
+//    private static final int PORT = 27017;
+//    private static final String DB_NAME = "teste";
+
+    private FachadaMongo() {
+    }
+
+    public static FachadaMongo getInstancia() {
+        if (instancia == null) {
+            instancia = new FachadaMongo();
+        }
+        return instancia;
+    }
+
+    public MongoDatabase getDB(String HOST, String PORT, String DB_NAME) {
+        int port = Integer.parseInt(PORT);
+        MongoClient mongoClient = new MongoClient(HOST, port);
+        MongoDatabase db = mongoClient.getDatabase(DB_NAME);
+        return db;
+    }
+
+    public MongoCollection getColecao(String HOST, String PORT, String DB_NAME, String colecao) {
+        MongoCollection col = FachadaMongo.getInstancia().getDB(HOST, PORT, DB_NAME).getCollection(colecao);
+        return col;
+    }
+
+    public void insert(String HOST, String PORT, String DB_NAME, Document documento) {
+        this.getColecao(HOST, PORT, DB_NAME, "documentos").insertOne(documento);
+    }
+}
