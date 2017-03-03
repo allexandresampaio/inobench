f017e1ac00bdd761ddb80262121bfaa1ce8e5335
allexandresampaio <allexandresss@gmail.com>
2017-03-03 16:46:05 -0300

    refatoração core

--- Inobench/src/CSVreader/LeituraCsv.java
+++ Inobench/src/CSVreader/LeituraCsv.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import inobench.Parametros;
+import core.Parametros;
 import org.bson.Document;
 
 /**
